a16894d993af1d9f38b1363382b23e7cea229894
lamnt59 <lamnt59@viettel.com.vn>
2023-07-05 14:43:16 +0700

    update

--- src/app/models/Student.js
+++ src/app/models/Student.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const Student = new Schema({
-    ho_ten: {type: String, max_length: 50, required: true},
-    ban_thi: {type: String, required: true},
-    khoi_thi: {type: String, max_length: 5, required: true},
+const student = new Schema({
+    hoten: {type: String, max_length: 50, required: true},
+    khoi: {type: String, max_length: 5, required: true},
+    ban: {type: String, required: true},
     toan: {type: Number, required: true},
     van: {type: Number, required: true},
     anh: {type: Number, required: true},
 });
 
-module.exports = mongoose.model('Student', Student);
+module.exports = mongoose.model('student', student);
